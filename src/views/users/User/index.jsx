8b87f5400b20a8932f19168c4b1fefb1cc8d31a4
lvzonglin <515124651@qq.com>
2018-04-04 09:52:41 +0800

    Update index.jsx

--- src/views/users/User/index.jsx
+++ src/views/users/User/index.jsx
@@ -8,15 +8,60 @@
  */
 import React,{Component} from 'react';
 
+import DataList from '@/components/DataList';
+
+import { iotUrl } from '@/api/url';
+
 class User extends Component{
   constructor(){
     super()
   }
 
   render(){
+    const listConfig = {
+      url:iotUrl.getUserUrl,
+      displayType:'table',
+      columns : [{
+        title: '序号',
+        dataIndex: 'id',
+        key: 'id'
+      },{
+        title: '用户名',
+        dataIndex: 'userName',
+        key: 'userName',
+        render: text => <a href="#">{text}</a>,
+      },{
+        title: '登录账号',
+        dataIndex: 'loginName',
+        key: 'loginName',
+      }, {
+        title: '用户状态',
+        dataIndex: 'state',
+        key: 'state',
+        render: (text, record) => {
+          if (text===true){
+            return '正常'
+          }else{
+            return '禁用'
+          }
+        }
+      },{
+        title: '创建时间',
+        dataIndex: 'creatTime',
+        key: 'creatTime',
+      }, {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a href="#">Delete</a>
+          </span>
+        ),
+      }]
+    }
     return (
       <div>
-        平台用户
+        <DataList {...listConfig}/>
       </div>
     )
   }
