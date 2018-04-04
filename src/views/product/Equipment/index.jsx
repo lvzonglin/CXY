0a20a31ba116cb2bfb47b95c1c06cb41f038efd9
lvzonglin <515124651@qq.com>
2018-04-04 09:51:09 +0800

    Update index.jsx

--- src/views/product/Equipment/index.jsx
+++ src/views/product/Equipment/index.jsx
@@ -7,6 +7,9 @@
  * Email  : 515124651@qq.com.
  */
 import React,{Component} from 'react';
+import DataList from '@/components/DataList';
+
+import { iotUrl } from '@/api/url';
 
 class Equipment extends Component{
   constructor(){
@@ -14,9 +17,35 @@ class Equipment extends Component{
   }
 
   render(){
+    const listConfig = {
+      url:iotUrl.getProData,
+      displayType:'table',
+      columns : [{
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a href="#">{text}</a>,
+      }, {
+        title: '描述',
+        dataIndex: 'description',
+        key: 'description',
+      }, {
+        title: '图片地址',
+        dataIndex: 'imageUrl',
+        key: 'imageUrl',
+      }, {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a href="#">Delete</a>
+          </span>
+        ),
+      }]
+    }
     return (
       <div>
-        设备管理
+        <DataList {...listConfig}/>
       </div>
     )
   }
